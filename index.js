e3f6281559b8e786ab9870a4b3688d7c10a3fac7
Marcus0224 <marcstlouis04@gmail.com>
2022-03-13 08:29:03 -0400

    finished Html format and getting it up and loaded into dist

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 
-const generatePage = require('./src/generateMarkdown');
+const generateHTML = require('./src/generateMarkdown');
 
 let team = [];
 let addManager = true;
@@ -47,110 +47,110 @@ const managerQuestions = [
     {
         type: 'input',
         name: 'officeNumber',
-        message:'What is manager Office number?'
+        message: 'What is manager Office number?'
     },
     {
         type: 'confirm',
         name: 'addNew',
-        message:'Would you like to add a member?'
+        message: 'Would you like to add a member?'
     },
 ];
 
 const engineerQuestions = [
-        {
-            type: 'input',
-            name: 'name',
-            message:'What is Engineer name?',
-            validate: (blank) => {
-                if (blank) {
-                    return true
-                } else {
-                    return 'Enter Engineer name.'
-                }
-            },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is Engineer name?',
+        validate: (blank) => {
+            if (blank) {
+                return true
+            } else {
+                return 'Enter Engineer name.'
+            }
         },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is Engineer ID?',
-            validate: (blank) => {
-                if (blank) {
-                    return true
-                } else {
-                    return 'Enter Engineer name.'
-                }
-            },
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is Engineer ID?',
+        validate: (blank) => {
+            if (blank) {
+                return true
+            } else {
+                return 'Enter Engineer name.'
+            }
         },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is Engineer Email?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is Engineer Email?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is Engineer Github username?',
+        validate: (blank) => {
+            if (blank) {
+                return true
+            } else {
+                return 'Enter Engineer Github username.'
+            }
         },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is Engineer Github username?',
-            validate: (blank) => {
-                if (blank) {
-                    return true
-                } else {
-                    return 'Enter Engineer Github username.'
-                }
-            },
-        },
-        {
-            type: 'confirm',
-            name: 'addNew',
-            message:'Would you like to add a member?'
-        },
-            
-    ];
+    },
+    {
+        type: 'confirm',
+        name: 'addNew',
+        message: 'Would you like to add a member?'
+    },
 
-    const internQuestions = [
-        {
-            type: 'input',
-            name: 'name',
-            message:'What is Intern name?',
-            validate: (blank) => {
-                if (blank) {
-                    return true
-                } else {
-                    return 'Enter Intern name.'
-                }
-            },
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is Intern name?',
+        validate: (blank) => {
+            if (blank) {
+                return true
+            } else {
+                return 'Enter Intern name.'
+            }
         },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is Intern ID?',
-            validate: (blank) => {
-                if (blank) {
-                    return true
-                } else {
-                    return 'Enter Intern Id.'
-                }
-            },
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is Intern ID?',
+        validate: (blank) => {
+            if (blank) {
+                return true
+            } else {
+                return 'Enter Intern Id.'
+            }
         },
-        {
-            type: 'input',
-            name: 'school',
-            message:'What school did Intern attend?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message:'What is email address of Intern?'
-        },
-        {
-            type: 'confirm',
-            name: 'addNew',
-            message:'Would you like to add a member?'
-        },
-    ];
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school did Intern attend?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is email address of Intern?'
+    },
+    {
+        type: 'confirm',
+        name: 'addNew',
+        message: 'Would you like to add a member?'
+    },
+];
 
 const selectMemberType = [
     {
-        type:'list',
+        type: 'list',
         name: 'memberType',
         message: 'Choose role for the Employee',
         choices: ['Manager', 'Engineer', 'Intern'],
@@ -159,89 +159,105 @@ const selectMemberType = [
 
 function addMember() {
     inquirer.prompt(selectMemberType)
-    .then(answer => {
-        if(answer.memberType === 'Manager') {
-            if (addManager) {
-                inquirer.prompt(managerQuestions)
-                .then(answer => {
-                    // employee info
-                    const manager = new Manager
-                    (
-                        answer.name,
-                        answer.id,
-                        answer.email,
-                        answer.officeNumber
-                    );
-                    team.push(manager);
-                    addManager = false;
-                    if (answer.addNew) {
-                       return addMember();
-                    } else {
-                        console.log(team);
-                        const template = generatePage(team);
-                        console.log(template)
-                        writeToFile('./dist/index.html', template);
-                    }
-                });
-            } else {
-                // 1 manager
-                console.log("There is a manger already!")
-                addMember();
+        .then(answer => {
+            if (answer.memberType === 'Manager') {
+                if (addManager) {
+                    inquirer.prompt(managerQuestions)
+                        .then(answer => {
+                            // employee info
+                            const manager = new Manager
+                                (
+                                    answer.name,
+                                    answer.id,
+                                    answer.email,
+                                    answer.officeNumber
+                                );
+                            team.push(manager);
+                            addManager = false;
+                            if (answer.addNew) {
+                                return addMember();
+                            } else {
+                                console.log(team);
+                                generateHTML(team)
+                                //const content = generateHTML(team);
+                                
+                                //console.log(template)
+                                //writeToFile('./dist/index.html', content);
+                            }
+                        });
+                } else {
+                    // 1 manager
+                    console.log("There is a manger already!")
+                    addMember();
+                }
+            } else if (answer.memberType === 'Engineer') {
+                inquirer.prompt(engineerQuestions)
+                    .then(answer => {
+                        //save info
+                        const engineer = new Engineer
+                            (
+                                answer.name,
+                                answer.id,
+                                answer.email,
+                                answer.github
+                            );
+                        //info to team array
+                        team.push(engineer);
+                        if (answer.addNew) {
+                            addMember();
+                        } else {
+                            console.log(team);
+                            generateHTML(team);
+                            //console.log(template)
+                            //writeToFile('./dist/index.html', template);
+                        };
+                    });
+            } else if (answer.memberType === 'Intern') {
+                inquirer.prompt(internQuestions)
+                    .then(answer => {
+                        //save info
+                        const intern = new Intern
+                            (
+                                answer.name,
+                                answer.id,
+                                answer.email,
+                                answer.school
+                            );
+                        //info to team array
+                        team.push(intern);
+                        if (answer.addNew) {
+                            addMember();
+                        } else {
+                            console.log(team);
+                            generateHTML(team);
+                            //console.log(template)
+                            //writeToFile('./dist/index.html', content);
+                        };
+                    });
             }
-        } else if (answer.memberType === 'Engineer') {
-            inquirer.prompt(engineerQuestions)
-            .then(answer => {
-                //save info
-                const engineer = new Engineer
-                (
-                    answer.name,
-                    answer.id,
-                    answer.email,
-                    answer.github
-                );
-                //info to team array
-                team.push(engineer);
-                if (answer.addNew) {
-                    addMember();
-                } else {
-                    console.log(team);
-                    const template = generatePage(team);
-                    console.log(template)
-                    writeToFile('./dist/index.html', template);
-                };
-            });
-        } else if (answer.memberType === 'Intern') {
-            inquirer.prompt(internQuestions)
-            .then(answer => {
-                //save info
-                const intern = new Intern
-                (
-                    answer.name,
-                    answer.id,
-                    answer.email,
-                    answer.school
-                );
-                //info to team array
-                team.push(intern);
-                if(answer.addNew) {
-                    addMember();
-                } else {
-                    console.log(team);
-                    const template = generatePage(team);
-                    console.log(template)
-                    writeToFile('./dist/index.html', template);
-                };
-            });
-        };
-    });
+        })
 };
 
-addMember ();
-
+addMember();
 
 // function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(),fileName), data);
-}
+//   function writeToFile(fileName, data) {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile('./dist/index.html', data, err => {
+//             if (err) {
+//                 reject(err)
+
+//                 return
+//             }
+
+//             resolve({
+//                 ok: true,
+//                 message: 'success'
+//             })
+//             //fs.writeFileSync(path.join(process.cwd(),fileName), data);
+//         })
+//     })
+// }   
+
 
 //Function to initialize app
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,6 +1,128 @@
-const generatePage = (team) => {
-    console.log('inside generate page function', team)
-    return `
+const fs = require('fs');
+
+function generatePage(team) {
+    //console.log('inside generate page function', team)
+    let teamContent
+
+    if (team.getRole() === 'Manager') {
+        teamContent = `<div class="card employee-card manager-card">
+        <div class="card-header text-center">
+            <h2 class="card-title">${team.getName()}</h2>
+            <h4 class="card-title">Title: ${team.getRole()}</h4>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group text-dark">
+                <li class="list-group-item">ID: ${team.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${team.getEmail()}">${team.getEmail()}</a></li>
+                <li class="list-group-item">Office number: <a href="tel:${team.getOfficeNumber()}">${team.getOfficeNumber()}</a></li>
+            </ul>
+        </div>
+    </div>
+    `;
+    } else if(team.getRole() === 'Engineer') {
+        teamContent = `  <div class="card employee-card engineer-card">
+        <div class="card-header text-center">
+            <h2 class="card-title">${team.getName()}</h2>
+            <h4 class="card-title">Title: ${team.getRole()}</h4>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group text-dark">
+                <li class="list-group-item">ID: ${team.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${team.getEmail()}">${team.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${team.getGithub()}" target="_blank" rel="noopener noreferrer">${team.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>   
+    `;
+    } else if(team.getRole() === 'Intern') {
+        teamContent = `  <div class="card employee-card intern-card">
+        <div class="card-header text-center">
+            <h2 class="card-title">${team.getName()}</h2>
+            <h4 class="card-title">Title: ${team.getRole()}</h4>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group text-dark">
+                <li class="list-group-item">ID: ${team.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${team.getEmail()}">${team.getEmail()}</a></li>
+                <li class="list-group-item">School: ${team.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    }
+    return teamContent
+  }
+
+    function generateHTML(data) {
+        console.log('this is data' + data)
+
+        let generate = ''
+        data.forEach(employee => {
+            console.log(employee)
+            generate += generatePage(employee)
+            
+        })
+
+
+        let content = 
+        `
+        <!DOCTYPE html> 
+        <html lang="en">
+    
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profile Generator</title>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        </head>
+      
+        <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 jumbotron text-white mb-3">
+                        <h1 class="text-center">Team</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="main-section col-12 d-flex justify-content-center">
+                        ${generate}
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>
+        `
+        console.log('this is content' + content)
+        writeToFile('./dist/index.html', content);
+
+    }
+
+    function writeToFile(fileName, data) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile('./dist/index.html', data, err => {
+                if (err) {
+                    reject(err)
+    
+                    return
+                }
+    
+                resolve({
+                    ok: true,
+                    message: 'success'
+                })
+                //fs.writeFileSync(path.join(process.cwd(),fileName), data);
+            })
+        })
+    }   
+
+    module.exports = generateHTML
+
+    /* return `
     <!DOCTYPE html> 
     <html lang="en">
 
@@ -25,7 +147,7 @@ const generatePage = (team) => {
         <div class="container">
             <div class="row">
                 <div class="main-section col-12 d-flex justify-content-center">
-                    ${createProfile(team)}
+                    ${createProfile}
                 </div>
             </div>
         </div>
@@ -34,8 +156,11 @@ const generatePage = (team) => {
     `;
 };
 
+
+
+
 // Create Team Profile
-const createProfile = team => {
+const createProfile = () => {
 
     // Create Manager Profile
     const createManager = manager => {
@@ -94,4 +219,35 @@ const createProfile = team => {
         `;
     };
 
-    module.exports = generatePage;
+    //push array to page
+    const html = [];
+    
+    // html.push(team
+    // //    .filter(employee => employee.getRole() === 'Manager')
+    // //   .map(manager => createManager(manager))
+    // );
+    let manager = team.filter( employee => employee.getRole() === 'Manager')
+    console.log('105'+ manager);
+    manager.map(manager => createManager(manager))
+    html.push(manager)
+    console.log(html +'108 ')
+    // html.push(team
+    //     .filter(employee => employee.getRole() === 'Engineer')
+    //     .map(engineer => createEngineer(engineer))
+    //     .join("")
+    // );
+    // html.push(team
+    //     .filter(employee => employee.getRole() === 'Intern')
+    //     .map(intern => createIntern(intern))
+    //     .join("")
+    // );
+
+   // return html; */
+   
+
+
+   
+
+   
+
+    
